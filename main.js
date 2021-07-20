f98fdd10a0c3030a787b414ae692c056f46aade9
Luciano <lucco6823@gmail.com>
2021-07-20 14:34:23 +0200

    Inserimento cognome nell'array

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+// 1. chiedi all’utente il cognome
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 var cognome = prompt('Inserisci il tuo cognome: ');
+var array_cognomi=["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+
+array_cognomi.push(cognome);
+console.log(array_cognomi);
