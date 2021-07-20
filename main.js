b43642d516bdf1a3577b8f79f1f8902f82da65ed
Luciano <lucco6823@gmail.com>
2021-07-20 14:57:18 +0200

    Eserciozio completo

--- main.js
+++ main.js
@@ -3,9 +3,16 @@
 // 3. stampa la lista ordinata alfabeticamente
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 var cognome = prompt("Inserisci il tuo cognome: ");
-var array_cognomi=["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+var array_cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+var posizione = 0;
 
 array_cognomi.push(cognome);
-console.log(array_cognomi);
+console.log("Disordinati: " + array_cognomi);
 
+console.log("Ordine alfabetico: " + array_cognomi.sort());
 document.getElementById("surnames").innerHTML = array_cognomi.sort();
+
+posizione = array_cognomi.indexOf(cognome);
+console.log("Posizione all'interno dell'array: " + posizione);
+console.log("Posizione all'interno delle lista cognomi: " + (posizione + 1));
+document.getElementById("position").innerHTML = posizione + 1;
