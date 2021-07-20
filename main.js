43290484e382227a5f5b6d1684abe61a0baeeaf5
Luciano <lucco6823@gmail.com>
2021-07-20 14:44:11 +0200

    Stampa in ordine alfabetico

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 // 3. stampa la lista ordinata alfabeticamente
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-var cognome = prompt('Inserisci il tuo cognome: ');
+var cognome = prompt("Inserisci il tuo cognome: ");
 var array_cognomi=["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
 array_cognomi.push(cognome);
 console.log(array_cognomi);
+
+document.getElementById("surnames").innerHTML = array_cognomi.sort();
